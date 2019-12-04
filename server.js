05d62b5defc562e7c557f60ec0ecc507e0c07352
Zack Bumpous <upwork.ztb@gmail.com>
2019-12-04 14:23:43 -0600

    Set up the server and the routes imported

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 
 const server = express();
+const userRoute = require('./users/userRouter');
+const postRoute = require('./posts/postRouter');
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
