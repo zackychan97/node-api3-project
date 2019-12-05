54705c6e85639a1159c084079aee39b1c29137a8
Zack Bumpous <upwork.ztb@gmail.com>
2019-12-05 16:38:11 -0600

    Adds custom middleware to userRouter.js file

--- users/userRouter.js
+++ users/userRouter.js
@@ -2,6 +2,9 @@ const express = require('express');
 
 const router = express.Router();
 
+const UserDB = require('./userDb.js');
+const PostDB = require('../posts/postDb.js');
+
 router.post('/', (req, res) => {
   // do your magic!
 });
@@ -35,14 +38,41 @@ router.put('/:id', (req, res) => {
 
 function validateUserId(req, res, next) {
   // do your magic!
+  const id = req.params.id
+  UserDB.getById(id)
+    .then(id => {
+      req.user = id
+    })
+    .catch(() => {
+      res.status(400).json({ message: "user id dont exist sonny" })
+    })
+    next();
 }
 
 function validateUser(req, res, next) {
   // do your magic!
+  const infoOfUser = req.body;
+
+  if(Object.values(infoOfUser).length <= 0) {
+    res.status(400).json({ message: "you dont have any info for us" })
+  } else if (!infoOfUser.name) {
+    res.status(400).json({ message: "we need the name" })
+  } else {
+    next();
+  }
 }
 
 function validatePost(req, res, next) {
   // do your magic!
+  const postsBody = req.body
+  if(Object.values(postsBody).length <= 0){
+    res.status(400).json({ message: "you dont have any info for us" })
+  } else if (!postsBody.text) {
+    res.status(400).json({ message: "we need the name" })
+  } else {
+    next();
+  }
+
 }
 
 module.exports = router;
